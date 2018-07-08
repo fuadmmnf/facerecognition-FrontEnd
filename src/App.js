9160e828e3c1bf0c3266e779110126bff3406746
fuadmmnf <bsse0920@iit.du.ac.bd>
2018-07-08 15:28:10 +0600

    navigation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <Navigation />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Navigation = () =>{
+	return(
+		<nav style= {{display: 'flex' , justifyContent:'flex-end'}}>
+			<p className='f4 link dim white pointer underline pa3 grow'>Sign Out </p>
+		</nav>
+	);
+}
+
+
+
+export default Navigation;
